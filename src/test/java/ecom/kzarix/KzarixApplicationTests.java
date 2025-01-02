32844dd4601a7b1362348eb744b42061c9a59a90
Med KTITE <ktite.m3@gmail.com>
2025-01-02 23:20:16 +0100

    first commit

---
+++ src/test/java/ecom/kzarix/KzarixApplicationTests.java
@@ -0,0 +1,13 @@
+package ecom.kzarix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KzarixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
