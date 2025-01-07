143467888796065536f64aa14c72e33c0c51ceb6
Med KTITE <ktite.m3@gmail.com>
2025-01-07 19:51:30 +0100

    📦✨ Add DTO package with LoginUserDto, RegisterUserDto, and VerifyUserDto 📨🔐

---
+++ src/main/java/ecom/kzarix/dto/RegisterUserDto.java
@@ -0,0 +1,12 @@
+package ecom.kzarix.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterUserDto {
+    private String email;
+    private String password;
+    private String username;
+}
---
+++ src/main/java/ecom/kzarix/dto/VerifyUserDto.java
@@ -0,0 +1,6 @@
+package ecom.kzarix.dto;
+
+public class VerifyUserDto {
+    private String email;
+    private String VerificationCode;
+}
